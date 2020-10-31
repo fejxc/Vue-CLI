3c6ad017df7069248d1a35ca04dc36f582dfcddf
fejxc <1031364436@qq.com>
2020-10-31 15:58:06 +0800

    假数据测试完成，还未和springboot后端结合

---
+++ element_users/src/components/Index.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <template>
+        <el-carousel indicator-position="outside">
+          <el-carousel-item v-for="item in imgs" :key="item">
+            <el-image
+              style="width: 100%; height: 100%"
+              :src="item"
+              fit="fil"></el-image>
+          </el-carousel-item>
+        </el-carousel>
+      </template>
+    </div>
+
+</template>
+
+<script>
+    import homeImg1 from "../assets/indexImges/1.jpg";
+    import homeImg2 from "../assets/indexImges/2.jpg";
+    import homeImg3 from "../assets/indexImges/3.jpg";
+    import homeImg4 from "../assets/indexImges/4.jpg";
+    import homeImg5 from "../assets/indexImges/5.jpg";
+    import homeImg6 from "../assets/indexImges/6.jpg";
+
+    export default {
+        name: "Index",
+        data(){
+            return{
+                imgs:[homeImg1,homeImg2,homeImg3,homeImg4,homeImg5,homeImg6]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- element_users/src/router/index.js
+++ element_users/src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Index from "../components/Index";
+import List from "../components/user/List";
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    {path:'/',redirect:'/index'},
+    {path:'/index', component:Index},
+    {path:'/users', component:List},
+  ],
 })
